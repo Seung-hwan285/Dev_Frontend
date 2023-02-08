80c10394fb31033cbf886bdcd75510d5f58f9fba
Seung-hwan285 <ghks123285@gmail.com>
2023-02-08 19:58:22 +0900

    chore: init projects

---
+++ new-app/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css'
+import React from 'react'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  // fds
+  return (
+    <html lang='en'>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
